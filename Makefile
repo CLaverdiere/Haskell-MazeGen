cff17fbf908879bd1d388b2b510fe77e40a53c5a
Chris Laverdiere <cmlaverdiere@gmail.com>
2014-12-31 16:41:08 -0500

    Color variants.

--- Makefile
+++ Makefile
@@ -5,3 +5,7 @@ data.txt: MazeGen.hs
 	runhaskell MazeGen.hs > data.txt
 
 all: maze.png
+
+clean:
+	rm data.txt
+	rm maze.png
--- draw.py
+++ draw.py
@@ -1,8 +1,9 @@
+from random import shuffle
 import Image, ImageDraw, math
 import sys
 
-# Draws maze given dimensions and coordinates of 4-tuple format:
-# (width, height) [(x1, y1, x2, y2), ...]
+# Draws maze given coordinates of 4-tuple format:
+# [(x1, y1, x2, y2), ...]
 
 w, h = eval(sys.argv[1])
 coords = eval(sys.argv[2])
@@ -13,12 +14,19 @@ scale = dim / ((w+h) / 2.)
 img = Image.new("RGB", (dim, dim))
 draw = ImageDraw.Draw(img)
 
-r = lambda x, y: int(x + y) / ((dim / 256) * 2)
+# Use a somewhat random color scheme.
+a = lambda x, y: 100
+b = lambda x, y: 200
+c = lambda x, y: int(x + y) / ((dim / 256) * 2)
+cs = [a,b,c]
+shuffle(cs)
 
 for coord in coords:
     x1, y1, x2, y2 = [x * scale for x in coord]
     draw.line((x1, y1, x2, y2), width=3,
-              fill="rgb({}, 100, 200)".format(r(x1,y1)))
+              fill="rgb({}, {}, {})".format(
+                  cs[0](x1,y1), cs[1](x1,y1), cs[2](x1,y1))
+              )
 
 del draw
 img.save("maze.png");
